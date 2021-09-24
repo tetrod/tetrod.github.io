e0bfe8b940c944458fd6c4ef1d80bb44a7df4cb1
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2021-09-23 22:52:27 -0700

    more tricks

--- bin/deploy.js
+++ bin/deploy.js
@@ -5,9 +5,7 @@
 const githubPagesDeployAction = require('@jamesives/github-pages-deploy-action');
 
 githubPagesDeployAction.default({
-  token: process.env.ACCESS_TOKEN,
   branch: 'gh-pages',
   folder: 'build',
-  repositoryName: 'tetrod/portal',
   workspace: 'src/project/location'
 });
